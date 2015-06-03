d7b9cc304c5673a2c812943b743cf8a667da0eaa
rupantar.nit@gmail.com <rupantar.nit@gmail.com>
2015-06-03 16:12:03 -0700

    Added the client sidebar to the  app

--- ui.R
+++ ui.R
@@ -1,13 +1,16 @@
 
 
 library(shiny)
-
+# Adding client names 
+clients= c("Reeds", "Toyota", "Zenni", "Verizon","Monster","Honeywell")
 shinyUI(fluidPage(
 
   # Application title
-  titlePanel("Social Reporting")
+  titlePanel("Social Reporting"),
+  #creating the sidebar Panel for the client name input
+  sidebarPanel(
+    selectInput("client","Select Client",clients))
+    
 )
-
-  #
 )
   
