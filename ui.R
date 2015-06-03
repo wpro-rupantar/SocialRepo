a53144c90b6c743f918a9826e6ffa8c7c99ac216
rupantar.nit@gmail.com <rupantar.nit@gmail.com>
2015-06-03 16:41:08 -0700

    Added the plot and the report tabs

--- ui.R
+++ ui.R
@@ -10,8 +10,17 @@ shinyUI(fluidPage(
   #creating the sidebar Panel for the client name input
   sidebarPanel(
     selectInput("client","Select Client",clients)),
+  # sidebar panel for the date range input
   dateRangeInput("dates",label=h3("Enter the date range"),
-  min="2015-01-01",max="2015-12-31",start="2015-01-01",end="2015-12-31")
- )    
+                 min="2015-01-01",max="2015-12-31",start="2015-01-01",end="2015-12-31")
+,
+
+#main Panel
+mainPanel(
+  tabsetPanel(type="tab",
+              tabPanel("Reports"),
+              tabPanel("Plots"))
+)
+)
 )
 
