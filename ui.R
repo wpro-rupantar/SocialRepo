44acaf74b8e32d4e9f273559ac0ccbd894d46a45
rupantar.nit@gmail.com <rupantar.nit@gmail.com>
2015-06-03 16:45:08 -0700

    Changed the sidebar layout

--- ui.R
+++ ui.R
@@ -9,11 +9,11 @@ shinyUI(fluidPage(
   titlePanel("Social Reporting"),
   #creating the sidebar Panel for the client name input
   sidebarPanel(
-    selectInput("client","Select Client",clients)),
+    selectInput("client","Select Client",clients),
   # sidebar panel for the date range input
   dateRangeInput("dates",label=h3("Enter the date range"),
                  min="2015-01-01",max="2015-12-31",start="2015-01-01",end="2015-12-31")
-,
+),
 
 #main Panel
 mainPanel(
