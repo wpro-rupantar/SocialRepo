219096ae531c86f8bd98cd7e90259adeb83646e9
rupantar.nit@gmail.com <rupantar.nit@gmail.com>
2015-06-05 15:11:36 -0700

    Display the top location by likes and made it dynamic with date

--- server.R
+++ server.R
@@ -8,7 +8,7 @@ library(dplyr)
 
 # initializing the server
 shinyServer(function(input,output){
-  ### loading and cleaning the location and likes data
+### loading and cleaning the location and likes data
   data1<-read.csv("/Users/rupantarrana/Project/social.reporting/Rshiny/data/wpro2015.csv")
   
   # parsing the date for the data
@@ -21,41 +21,53 @@ shinyServer(function(input,output){
   n=nrow(data1)
   data1=data1[-c(n-1,n),]
   
+## data cleaning is done 
+ # making reactive selection of the data
+  
   data2r=reactive({
     # Extracting date range from the user input
     a<-as.POSIXct(input$dates[1]) 
     b<-as.POSIXct(input$dates[2]) 
-    df2= data1 %>%
+    
+   df2<-data1 %>%
       filter(Date > a & Date < b)
-    df2
+   # selecting the first and the last from the selected data
+   df2<-as.data.frame(t(df2[c(1,nrow(df2)),]))
+   
+   colnames(df2)= c("start_likes","end_likes")
+   df2 = df2[-1,]
+   # remove the NAs
+   data3=na.omit(df2)
+   # create new fields for city and state
+   data3$state = str_sub(rownames(data3),-2)
+   data3
+   data3$city = str_extract(rownames(data3),"[aA-zZ]+.?[aA-zZ]+")
+   data3$new_likes = as.integer(data3$end_likes) - as.integer(data3$start_likes)
+   rownames(data3)= NULL
+   select(data3,state,city,start_likes,end_likes,new_likes)%>%
+     arrange(desc(new_likes))
+  
   })
   
+# display the output of the top 10
   output$summ<-renderTable({
-    datar<-as.data.frame(data2r())
-    datar
+    data2r()[1:10,]
+      
   })
-    
-    
-  data3=as.data.frame(t(data2[-1]))
-  colnames(data3)=c("start_likes","end_likes")
-  data3$New_Likes = data3$end_likes-data3$start_likes
-  # remove the NAs
-  data3=na.omit(data3)
+
   
-  # create new fields for city and state
-  data3$state = str_sub(rownames(data3),-2)
-  data3$city = str_extract(rownames(data3),"[aA-zZ]+.?[aA-zZ]+")
-  # state summary
-  data4= ddply(data3,~state,summarize,Total.new.likes=sum(New_Likes))
-  data4= arrange(data4,desc(Total.new.likes))
-  data4
-  
-  # city summary
-  
-  data5= ddply(data3,~city,summarize,Total.new.likes=sum(New_Likes))
-  data5= arrange(data5,desc(Total.new.likes))
-  data5
-  
-  #rendering
-  
+
+  #   # state summary
+  #   data4= ddply(data3,~state,summarize,Total.new.likes=sum(New_Likes))
+  #   data4= arrange(data4,desc(Total.new.likes))
+  #   data4
+  #   
+  #   # city summary
+  #   
+  #   data5= ddply(data3,~city,summarize,Total.new.likes=sum(New_Likes))
+  #   data5= arrange(data5,desc(Total.new.likes))
+  #   data5
+  #   
+  #   #rendering
+  #   
 })
